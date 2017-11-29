1836eea24d3c482827a04000ffad04ad64e9a25a
Billgo <cocobill@vip.qq.com>
2017-11-29 15:50:27 +0800

    update

---
+++ example_test.go
@@ -0,0 +1,36 @@
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package flag_test
+
+import (
+	"fmt"
+
+	"github.com/gostores/flag"
+)
+
+func ExampleShorthandLookup() {
+	name := "verbose"
+	short := name[:1]
+
+	flag.BoolP(name, short, false, "verbose output")
+
+	// len(short) must be == 1
+	flag := flag.ShorthandLookup(short)
+
+	fmt.Println(flag.Name)
+}
+
+func ExampleFlagSet_ShorthandLookup() {
+	name := "verbose"
+	short := name[:1]
+
+	fs := flag.NewFlagSet("Example", flag.ContinueOnError)
+	fs.BoolP(name, short, false, "verbose output")
+
+	// len(short) must be == 1
+	flag := fs.ShorthandLookup(short)
+
+	fmt.Println(flag.Name)
+}
